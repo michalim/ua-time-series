9cd745647ef00e5cfdd2869fe0486ec28d009eff
deerslyr1 <deerslyr1@0b11da24-fda1-732a-1f35-77cfe01f0a1f>
2011-08-10 18:23:19 +0000

    Added a function to perform Darpa Evaluations when we have an actual test set that is different than the training set.

--- src/edu/arizona/cs/learn/timeseries/experiment/KinectExperiments.java
+++ src/edu/arizona/cs/learn/timeseries/experiment/KinectExperiments.java
@@ -1,34 +1,60 @@
 package edu.arizona.cs.learn.timeseries.experiment;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.apache.commons.math.stat.descriptive.SummaryStatistics;
 
 import edu.arizona.cs.learn.timeseries.classification.Classifier;
 import edu.arizona.cs.learn.timeseries.classification.Classify;
+import edu.arizona.cs.learn.timeseries.classification.ClassifyCallable;
 import edu.arizona.cs.learn.timeseries.classification.ClassifyParams;
 import edu.arizona.cs.learn.timeseries.evaluation.BatchStatistics;
-import edu.arizona.cs.learn.timeseries.evaluation.CrossValidation;
 import edu.arizona.cs.learn.timeseries.evaluation.LeaveOneOut;
-import edu.arizona.cs.learn.timeseries.evaluation.SplitAndTest;
 import edu.arizona.cs.learn.timeseries.model.Instance;
 import edu.arizona.cs.learn.timeseries.model.SequenceType;
 import edu.arizona.cs.learn.util.Utils;
 
 /**
- * These experiments are run on the data prepared for
- * me by Federico and have been gathered from the brain
- * hats that Carole owns.
+ *
  * @author wkerr
  *
  */
 public class KinectExperiments {
 
+	/**
+	 * 
+	 * @param map
+	 * @param count
+	 * @return
+	 */
+	public static Map<String,List<Instance>> subset(Map<String,List<Instance>> map, int count) { 
+
+		Map<String,List<Instance>> sMap = new HashMap<String,List<Instance>>();
+		
+		for (String key : map.keySet()) { 
+			List<Instance> instances = map.get(key);
+			if (instances.size() < count)
+				continue;
+			
+			Collections.shuffle(instances);
+			List<Instance> sInstances = new ArrayList<Instance>();
+			for (int i = 0; i < count; ++i) { 
+				sInstances.add(instances.get(i));
+			}
+			
+			sMap.put(key, sInstances);
+		}
+		
+		return sMap;
+	}
+	
 	/**
 	 * Calculate the performance of the CAVE classifier on
 	 * the kinect data....
@@ -45,22 +71,32 @@ public class KinectExperiments {
 	 * @param prefix
 	 */
 	public static void performance() { 
-		Map<String,List<Instance>> data = Utils.load("kinect", SequenceType.allen);
+		Utils.LIMIT_RELATIONS = true;
+		Utils.WINDOW = 5;
+		
+		SequenceType type = SequenceType.allen;
+		
+		Map<String,List<Instance>> data = Utils.load("/Users/wkerr/psi/maria/fluents/", "Deva", type);
+		data = subset(data, 10);
+		
 		List<String> classNames = new ArrayList<String>(data.keySet());
 		Collections.sort(classNames);
 		
 		ClassifyParams params = new ClassifyParams();
-		params.type = SequenceType.allen;
+		params.type = type;
+
 		// Vary the prunePct to get different results.
-		params.prunePct = 0.5;
-		params.incPrune = false;
-		Classifier c = Classify.prune.getClassifier(params);
+//		params.prunePct = 0.5;
+//		params.incPrune = true;
+//		Classifier c = Classify.prune.getClassifier(params);
 
 		// Comment out the above line and comment in this line
 		// to use nearest neighbor classification instead of CAVE classification.
-//		params.k = 1;
-//		Classifier c = Classify.knn.getClassifier(params);
+		params.k = 1;
+		Classifier c = Classify.knn.getClassifier(params);
 
+//		SplitAndTest sat = new SplitAndTest(10, 2.0/3.0);
+//		List<BatchStatistics> stats = sat.run(System.currentTimeMillis(), classNames, data, c);
 		LeaveOneOut loo = new LeaveOneOut();
 		List<BatchStatistics> stats = loo.run(System.currentTimeMillis(), classNames, data, c);
 		
@@ -90,6 +126,45 @@ public class KinectExperiments {
 	
 	public static void main(String[] args) throws Exception { 
 		performance();
+	}
+	
+	public static void darpaEvaluation() {
+		Utils.LIMIT_RELATIONS = true;
+		Utils.WINDOW = 5;
+		
+		SequenceType type = SequenceType.allen;
+		Map<String,List<Instance>> training = Utils.load("<directory>", "<prefix>", type);
+		Map<String,List<Instance>> test = Utils.load("<directory>", "<prefix>", type);
+		
+		List<String> classNames = new ArrayList<String>(training.keySet());
+		Collections.sort(classNames);
+		
+		ClassifyParams params = new ClassifyParams();
+		params.type = type;
+		params.k = 1;
+		Classifier c = Classify.knn.getClassifier(params);
+
+		// Perform the training.
+		c.train(training);
+		
+		// Now let's do a test....
+		ExecutorService execute = Executors.newFixedThreadPool(Utils.numThreads);;
+
+		BatchStatistics fs = new BatchStatistics(c.getName(), classNames);
+		List<Future<ClassifyCallable>> futureList = new ArrayList<Future<ClassifyCallable>>();
+		for (List<Instance> instances : test.values())
+			for (Instance instance : instances) 
+				futureList.add(execute.submit(new ClassifyCallable(c, instance)));
+		
+		for (Future<ClassifyCallable> future : futureList) {
+			try {
+				ClassifyCallable results = future.get();
+				fs.addTestDetail(results.actual(), results.predicted(), results.duration());
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+		}	
+		System.out.println("Accuracy: " + fs.accuracy());
 	}
 	
 }
