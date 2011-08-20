eb00c900fa28d2d460c86ab30136e1d1d03a0def
deerslyr1 <deerslyr1@0b11da24-fda1-732a-1f35-77cfe01f0a1f>
2011-08-20 19:46:40 +0000

    Sending back to Maria

--- src/edu/arizona/cs/learn/timeseries/experiment/KinectExperiments.java
+++ src/edu/arizona/cs/learn/timeseries/experiment/KinectExperiments.java
@@ -1,6 +1,9 @@
 package edu.arizona.cs.learn.timeseries.experiment;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -43,24 +46,24 @@ public class KinectExperiments {
 	public static Map<String,List<Instance>> subset(Map<String,List<Instance>> map, int count) { 
 
 		Map<String,List<Instance>> sMap = new HashMap<String,List<Instance>>();
-		
+
 		for (String key : map.keySet()) { 
 			List<Instance> instances = map.get(key);
 			if (instances.size() < count)
 				continue;
-			
+
 			Collections.shuffle(instances);
 			List<Instance> sInstances = new ArrayList<Instance>();
 			for (int i = 0; i < count; ++i) { 
 				sInstances.add(instances.get(i));
 			}
-			
+
 			sMap.put(key, sInstances);
 		}
-		
+
 		return sMap;
 	}
-	
+
 	/**
 	 * Calculate the performance of the CAVE classifier on
 	 * the kinect data....
@@ -81,35 +84,35 @@ public class KinectExperiments {
 		Utils.WINDOW = 5;
 
 		SequenceType type = SequenceType.allen;
-		
+
 		Map<String,List<Instance>> data = Utils.load("/data/input/", dataName, type);
-//		data = subset(data, 10);
-		
+		//		data = subset(data, 10);
+
 		List<String> classNames = new ArrayList<String>(data.keySet());
 		Collections.sort(classNames);
-		
+
 		ClassifyParams params = new ClassifyParams();
 		params.type = type;
 
 		// Vary the prunePct to get different results.
-//		params.prunePct = 0.5;
-//		params.incPrune = true;
-//		Classifier c = Classify.prune.getClassifier(params);
+		//		params.prunePct = 0.5;
+		//		params.incPrune = true;
+		//		Classifier c = Classify.prune.getClassifier(params);
 
 		// Comment out the above line and comment in this line
 		// to use nearest neighbor classification instead of CAVE classification.
 		params.k = 1;
 		Classifier c = Classify.knn.getClassifier(params);
 
-//		SplitAndTest sat = new SplitAndTest(10, 2.0/3.0);
-//		List<BatchStatistics> stats = sat.run(System.currentTimeMillis(), classNames, data, c);
-//		LeaveOneOut loo = new LeaveOneOut();
-//		List<BatchStatistics> stats = loo.run(System.currentTimeMillis(), classNames, data, c);
-		
-		
+		//		SplitAndTest sat = new SplitAndTest(10, 2.0/3.0);
+		//		List<BatchStatistics> stats = sat.run(System.currentTimeMillis(), classNames, data, c);
+		//		LeaveOneOut loo = new LeaveOneOut();
+		//		List<BatchStatistics> stats = loo.run(System.currentTimeMillis(), classNames, data, c);
+
+
 		CrossValidation cv = new CrossValidation(5);
 		List<BatchStatistics> stats = cv.run(System.currentTimeMillis(), classNames, data, c);
-		
+
 		// For now let's print out some informative stuff and build
 		// one confusion matrix
 		SummaryStatistics perf = new SummaryStatistics();
@@ -121,7 +124,7 @@ public class KinectExperiments {
 
 			perf.addValue(accuracy);
 			System.out.println("Fold - " + i + " -- " + accuracy);
-			
+
 			for (int j = 0; j < classNames.size(); ++j) { 
 				for (int k = 0; k < classNames.size(); ++k) { 
 					matrix[j][k] += confMatrix[j][k];
@@ -129,12 +132,12 @@ public class KinectExperiments {
 			}
 		}
 		System.out.println("Performance: " + perf.getMean() + " sd -- " + perf.getStandardDeviation());
-		
+
 		// Now print out the confusion matrix (in csv format)
 		System.out.println(Utils.toCSV(classNames, matrix));
 
 	}
-	
+
 	public static void main(String[] args) throws Exception { 
 		String option=args[0];
 		String TrainName=args[1];
@@ -149,28 +152,39 @@ public class KinectExperiments {
 			darpaEvaluation(TrainName,TestName);
 		}	
 	}
-	
+
 	public static void darpaEvaluation(String trainName, String testName) {
 		Utils.LIMIT_RELATIONS = true;
 		Utils.WINDOW = 5;
-		
+
 		int k = 6;
-		
+
 		SequenceType type = SequenceType.allen;
-		Map<String,List<Instance>> trainingMap = Utils.load("data/input","DevaSmooth1" , type);
+		Map<String,List<Instance>> trainingMap = Utils.load("data/input","DevaSmooth2" , type);
 		// combine all of the training data into a single list
 		List<Instance> training = new ArrayList<Instance>();
 		for (List<Instance> tmp : trainingMap.values()) {
 			training.addAll(tmp);
 		}
-		
+
 		List<String> testingFiles = new ArrayList<String>();
-		testingFiles.add("data/input/T2C_10_LEAVE10_A1_C1_id1.prop18.lisp");		
-		testingFiles.add("data/input/T2C_10_LEAVE10_A1_C2_id1.prop10.lisp");	
+
+		for (File f : new File("data/input/").listFiles()) {
+
+			if ((f.getName().startsWith(testName)) && (f.getName().endsWith("lisp"))) {
+				System.out.println(f.getName());
+				System.out.println(f.getAbsolutePath());
+				testingFiles.add("data/input/"+f.getName());
+
+			}
+		}
+
+		//				testingFiles.add("data/input/T2C_10_LEAVE10_A1_C1_id1.prop18.lisp");		
+		//				testingFiles.add("data/input/T2C_10_LEAVE10_A1_C2_id1.prop10.lisp");	
 
 		// Now let's test all of the files.
 		ExecutorService execute = Executors.newFixedThreadPool(Utils.numThreads);;
-		
+
 		List<Future<ACallable>> futureList = new ArrayList<Future<ACallable>>();
 		for (String file : testingFiles) { 
 			// Based on conversations with Maria and Raquel -- There is only a single test per file.
@@ -178,24 +192,36 @@ public class KinectExperiments {
 			test.sequence(SequenceType.allen.getSequence(test.intervals()));
 			futureList.add(execute.submit(new ACallable(training, file, test, k)));
 		}
-		
-		for (Future<ACallable> future : futureList) {
-			try {
-				ACallable results = future.get();
-				
-				// Here is where you output the information that you want
-				// to output for each test....
-				System.out.print(results.file() + " ");
-				for (Distance d : results.results()) { 
-					// print out the class name and the distance to the class being tested
-					System.out.print(" " + d.instance.name() + " ");
-				}
-				System.out.println();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} 
-		}	
-		
+
+
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("WindowClass2"+System.currentTimeMillis()+".txt"));	   
+
+			for (Future<ACallable> future : futureList) {
+				try {
+					ACallable results = future.get();
+
+					// Here is where you output the information that you want
+					// to output for each test....
+					System.out.print(results.file() + "xxxxxx ");
+					out.write(results.file()+',');
+					for (Distance d : results.results()) { 
+						// print out the class name and the distance to the class being tested
+						System.out.print(" " + d.instance.name() + " ");
+						out.write(d.instance.name());	
+					}
+					out.write('\n');
+					System.out.println();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} 
+			}
+
+			out.close();
+
+		} catch (IOException e) {
+		}
+
 		// no computation of accuracy since there isn't really a test.
 	}
 }
@@ -212,19 +238,19 @@ class ACallable implements Callable<ACallable> {
 
 	private List<Instance> _training;
 	private Instance _test;
-	
+
 	private int _k;
-	
+
 	private List<Distance> _results;
-	
-	
+
+
 	public ACallable(List<Instance> training, String testFile, Instance test, int k) { 
 		_training = training;
 		_file = testFile;
 		_test = test;
 		_k = k;
 	}
-	
+
 	@Override
 	public ACallable call() throws Exception {
 		Params params = new Params();
@@ -247,13 +273,13 @@ class ACallable implements Callable<ACallable> {
 				return Double.compare(o1.d, o2.d);
 			}
 		});
-		
+
 		while (_results.size() > _k) { 
 			_results.remove(_results.size()-1);
 		}
 		return this;
 	} 
-	
+
 	/**
 	 * Return the file that contains this test instance.
 	 * @return
@@ -261,7 +287,7 @@ class ACallable implements Callable<ACallable> {
 	public String file() { 
 		return _file;
 	}
-	
+
 	/**
 	 * Return the results after computing the nearest neighbors.
 	 * @return
