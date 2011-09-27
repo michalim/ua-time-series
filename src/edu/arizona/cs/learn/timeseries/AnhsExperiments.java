0ee36ff78ef2ba9dc04ebd8d5a41b5f1f9684436
anhxuan.tran <anhxuan.tran@gmail.com>
2011-09-27 20:21:29 +0000

    Added a classification experiment.

--- src/edu/arizona/cs/learn/timeseries/AnhsExperiments.java
+++ src/edu/arizona/cs/learn/timeseries/AnhsExperiments.java
@@ -1,16 +1,26 @@
 package edu.arizona.cs.learn.timeseries;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import edu.arizona.cs.learn.algorithm.alignment.Similarity;
+import edu.arizona.cs.learn.timeseries.classification.Classifier;
+import edu.arizona.cs.learn.timeseries.classification.Classify;
+import edu.arizona.cs.learn.timeseries.classification.ClassifyParams;
+import edu.arizona.cs.learn.timeseries.experiment.Classification;
+import edu.arizona.cs.learn.timeseries.model.Instance;
 import edu.arizona.cs.learn.timeseries.model.SequenceType;
 import edu.arizona.cs.learn.timeseries.prep.ww2d.ExportStreamFile;
 import edu.arizona.cs.learn.timeseries.prep.ww2d.WubbleWorld2d;
 import edu.arizona.cs.learn.timeseries.recognizer.Recognizer;
 import edu.arizona.cs.learn.timeseries.recognizer.RecognizerStatistics;
+import edu.arizona.cs.learn.util.Utils;
 
 public class AnhsExperiments {
 	
@@ -18,14 +28,37 @@ public class AnhsExperiments {
 		int n = 30;
 		String[] activities = {"chase", "eat", "fight", "flee", "kick-ball", "kick-column"};
 		
-		// NOTE: uncomment the following lines to run the respective pre-processing
-//		exportWW2DStateDBToCSV(n, activities);
-//		extractWW2DFluents(n, activities);
+		boolean exportFromDB, extractFluents, runRecognition, runClassification;
+		exportFromDB = false;
+		extractFluents = false;
+		runRecognition = false;
+		runClassification = true;
 		
-		boolean setup = false;	/* only need to set up once, turn to false for subsequent runs */
-		runRecognitionExperiment("global-internal-ww2d", setup, 1 /* num of experiments */ );
+		if (exportFromDB)
+			exportWW2DStateDBToCSV(n, activities);
+		if (extractFluents)
+			extractWW2DFluents(n, activities);
+		if (runRecognition) {
+			boolean setup = false;	/* only need to set up once, turn to false for subsequent runs */
+			runRecognitionExperiment("global-internal-ww2d", setup, 1 /* num of experiments */ );
+		}
+		if (runClassification)
+			runClassificationExperiment("global-internal-ww2d");
 		
 		System.out.println("Done");
+	}
+	
+	private static void runClassificationExperiment(String prefix) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh-mm");
+		String fileName = "data/classification-" + prefix + "-" + dateFormat.format(new Date()) + ".csv";
+		
+		ClassifyParams rParams = new ClassifyParams();
+		rParams.prunePct = 0.5;
+		rParams.incPrune = true;
+		rParams.similarity = Similarity.strings;
+		Classifier c1 = Classify.prune.getClassifier(rParams);
+		Classification.performance(Utils.load(prefix, SequenceType.allen), c1, fileName);
+		
 	}
 	
 	@SuppressWarnings("unused")
